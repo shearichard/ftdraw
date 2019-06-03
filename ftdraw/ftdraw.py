a54bee97d812cc3dbddb92633acf52b777d7aa46
Richard Shea <rshea@thecubagroup.com>
2019-06-03 22:56:39 +1200

    Lines WIP

--- ftdraw/ftdraw.py
+++ ftdraw/ftdraw.py
@@ -71,14 +71,90 @@ def draw_rect_sandbox():
     wd = ht / 1.6
     max_x = 300 # 650
     max_y = 200 # 400
+    idx_x = 0
+    idx_y = 0
+    lst_arr_bx = []
     for x in range(50, max_x, math.floor(wd * 2.0)):
         for y in range(50, max_y, math.floor(ht * 1.2)):
-            print("x = ", x, ". y = ", y, ". ht = ", ht, ". wd = ", wd)
-            c.rect(x , y , ht , wd , stroke=1 , fill=0)
-            write_interior_text(c, (x + (wd*0.3)), (y + ht - (ht*0.7)))
+            if idx_x == 0:
+                print("x = ", x, ". y = ", y, ". ht = ", ht, ". wd = ", wd)
+                c.rect(x , y , ht , wd , stroke=1 , fill=0)
+                write_interior_text(c, (x + (wd*0.3)), (y + ht - (ht*0.7)))
+                lst_arr_bx.append({'x': x, 'y': y, 'ht': ht, 'wd': wd })
+            idx_y += 1
+        idx_x += 1
+
+    import pprint
+    pprint.pprint(lst_arr_bx)
+    draw_arr_sandbox(c, lst_arr_bx)
 
     c.showPage()
     c.save()
+
+def draw_arr_sandbox(c, lst_arr_bx):
+    '''
+    Draw lines linking each element of the
+    argument `lst_arr_bx` so that a line
+    joins 0-1, 1-2, 2-3 etc.
+    '''
+    idx = 0
+    first_bx = True
+
+    for idx in range(len(lst_arr_bx)):
+        print(idx)
+        if first_bx:
+            first_bx = False
+        else:
+            link_boxes(c, lst_arr_bx[idx-1], lst_arr_bx[idx])
+
+def link_boxes(c, start_bx, end_bx):
+    '''
+    Draws a line between the start box and the 
+    end box
+
+    An assumption is made that the first box
+    is above the second box
+    '''
+    start_x, start_y = get_start_on_box(start_bx)
+    end_x, end_y = get_end_on_box(end_bx)
+    print("")
+    print('{:d}/{:d} to {:d}/{:d} '.format(math.floor(start_x), math.floor(start_y), math.floor(end_x), math.floor(end_y)))
+
+    p = c.beginPath()
+
+    p.moveTo(start_x, start_y)
+    p.lineTo(end_x, end_y)
+
+    p.close()
+    c.drawPath(p)
+    
+
+
+def get_start_on_box(start_bx):
+    '''
+    Works out the x/y coordinates of
+    the point which is halfway along
+    the bottom line of the box
+    '''
+
+    out_y = start_bx['y'] + start_bx['ht']
+    out_x = start_bx['x'] + (0.5 * start_bx['wd'])
+
+    return out_x, out_y
+
+
+def get_end_on_box(end_bx):
+    '''
+    Works out the x/y coordinates of
+    the point which is halfway along
+    the top line of the box
+    '''
+
+    out_y = end_bx['y']
+    out_x = end_bx['x'] + (0.5 * end_bx['wd'])
+
+    return out_x, out_y
+
 
 def main():
 
