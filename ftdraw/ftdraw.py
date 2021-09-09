4982fe9d535a3e399ebaae7ea8018cb78ba558e2
Richard Shea <rshea@thecubagroup.com>
2021-09-09 20:05:58 +1200

    Use more descriptive name for reportlab canvas objects

--- ftdraw/ftdraw.py
+++ ftdraw/ftdraw.py
@@ -41,7 +41,7 @@ def textobject_demo():
     my_canvas.drawText(textobject)
     my_canvas.save()
 
-def write_interior_text(c, x, y):
+def write_interior_text(rl_cnv, x, y):
     '''
     Writes text in a given location. Intended
     to be used to write text into the middle
@@ -49,14 +49,14 @@ def write_interior_text(c, x, y):
     output
     '''
 
-    textobject = c.beginText()
+    textobject = rl_cnv.beginText()
 
     textobject.setTextOrigin(x, y)
     textobject.setFont('Times-Roman', 10)
     textobject.setFillColor(red)
     textobject.textLine(formatxy(x,y))
 
-    c.drawText(textobject)
+    rl_cnv.drawText(textobject)
 
 def draw_rect_sandbox():
     '''
@@ -66,7 +66,7 @@ def draw_rect_sandbox():
     The output document is hardcoded as A4 landscape.
     '''
 
-    c = canvas.Canvas(getoutputpath("txt_obj_test.pdf"), landscape(pagesize=A4))
+    rl_cnv = canvas.Canvas(getoutputpath("txt_obj_test.pdf"), landscape(pagesize=A4))
     ht = 40
     wd = ht * 1.6
     max_x = 300 # 650
@@ -78,20 +78,20 @@ def draw_rect_sandbox():
         for y in range(50, max_y, math.floor(ht * 1.2)):
             if idx_x == 0:
                 print("x = ", x, ". y = ", y, ". ht = ", ht, ". wd = ", wd)
-                c.rect(x , y , wd , ht , stroke=1 , fill=0)
-                write_interior_text(c, (x + (wd*0.3)), (y + ht - (ht*0.7)))
+                rl_cnv.rect(x , y , wd , ht , stroke=1 , fill=0)
+                write_interior_text(rl_cnv, (x + (wd*0.3)), (y + ht - (ht*0.7)))
                 lst_arr_bx.append({'x': x, 'y': y, 'ht': ht, 'wd': wd })
             idx_y += 1
         idx_x += 1
 
     import pprint
     pprint.pprint(lst_arr_bx)
-    draw_arr_sandbox(c, lst_arr_bx)
+    draw_arr_sandbox(rl_cnv, lst_arr_bx)
 
-    c.showPage()
-    c.save()
+    rl_cnv.showPage()
+    rl_cnv.save()
 
-def draw_arr_sandbox(c, lst_arr_bx):
+def draw_arr_sandbox(rl_cnv, lst_arr_bx):
     '''
     Draw lines linking each element of the
     argument `lst_arr_bx` so that a line
@@ -105,9 +105,9 @@ def draw_arr_sandbox(c, lst_arr_bx):
         if first_bx:
             first_bx = False
         else:
-            link_boxes(c, lst_arr_bx[idx-1], lst_arr_bx[idx])
+            link_boxes(rl_cnv, lst_arr_bx[idx-1], lst_arr_bx[idx])
 
-def link_boxes(c, start_bx, end_bx):
+def link_boxes(rl_cnv, start_bx, end_bx):
     '''
     Draws a line between the start box and the 
     end box
@@ -120,13 +120,13 @@ def link_boxes(c, start_bx, end_bx):
     print("")
     print('{:d}/{:d} to {:d}/{:d} '.format(math.floor(start_x), math.floor(start_y), math.floor(end_x), math.floor(end_y)))
 
-    p = c.beginPath()
+    p = rl_cnv.beginPath()
 
     p.moveTo(start_x, start_y)
     p.lineTo(end_x, end_y)
 
     p.close()
-    c.drawPath(p)
+    rl_cnv.drawPath(p)
     
 
 
